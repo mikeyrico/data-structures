05ed93d604935286fe791be99cc9f1d8ccc55ac4
Michael Rico <rico.michael@gmail.com>
2016-07-28 23:42:35 -0700

    Implement binary search, with test

--- binary-search/bs.js
+++ binary-search/bs.js
@@ -1,6 +1,22 @@
-function binarySearch(arr, target) {
-  return 'running';
+function binarySearch(arr, target, i, j) {
+  i = i === undefined ? 0 : i;
+  j = j === undefined ? arr.length - 1 : j;
+  var midIndex = getMid(i, j);
+  var mid = arr[midIndex];
+  if (target === mid) {
+    return true;
+  } else if (i >= j) {
+    return false;
+  } else if (mid < target) {
+    return binarySearch(arr, target, midIndex+1, j);
+  } else {
+    return binarySearch(arr, target, 0, midIndex - 1);
+  }
 };
+
+function getMid(a, b) {
+  return Math.floor((a+b)/2);
+}
 
 module.exports = {
   binarySearch: binarySearch,
--- binary-search/bs.test.js
+++ binary-search/bs.test.js
@@ -1,8 +1,18 @@
 var expect = require('chai').expect;
 var binarySearch = require('./bs').binarySearch;
 
+
+
 describe('binary search', function() {
+  // beforeEach(function() {
+  //   var test = [2, 7, 8, 23, 45, 78, 98];
+  // });
+
   it('should find the target', function() {
-    expect(binarySearch([2, 7, 8, 23, 45, 78, 98])).to.equal('running');
+    expect(binarySearch([2, 7, 8, 23, 45, 78, 98], 78)).to.equal(true);
+  });
+
+  it('should return false if the target is not in the input', function() {
+    expect(binarySearch([2, 7, 8, 23, 45, 78, 98], 1)).to.equal(false);
   });
 });
