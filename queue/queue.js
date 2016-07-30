dcd4647a0a5d020111cae6e01e0abe0d25398b74
Michael Rico <rico.michael@gmail.com>
2016-07-30 12:16:40 -0700

    Implement functionally instantiated Queue

--- queue/queue.js
+++ queue/queue.js
@@ -1,35 +1,36 @@
 module.exports = (function () {
 
   var Queue = function() {
-    var instance = Object.create(queueMethods);
-    instance._start = 0;
-    instance._end = 0;
-    instance._storage = {};
+    var instance = {};
+    var start = 0;
+    var end = 0;
+    var storage = {};
+
+    instance.enqueue = function(item) {
+      storage[end++] = item;
+    };
+
+    instance.dequeue = function() {
+      if (!this.size()) {
+        return;
+      }
+      var result = storage[start];
+      delete storage[start];
+      start++;
+      return result;
+    };
+
+    instance.size = function() {
+      return end - start;
+    };
+
+    instance.peek = function() {
+      return storage[start];
+    }
+
     return instance;
   };
 
-  var queueMethods = {};
-
-  queueMethods.enqueue = function(item) {
-    this._storage[this._end++] = item;
-  };
-
-  queueMethods.dequeue = function() {
-    if (!this.size()) {
-      return;
-    }
-    var result = this._storage[this._start];
-    delete this._storage[this._start++];
-    return result;
-  };
-
-  queueMethods.size = function() {
-    return this._end - this._start;
-  };
-
-  queueMethods.peek = function() {
-    return this._storage[this._start];
-  };
 
   return Queue;
 
