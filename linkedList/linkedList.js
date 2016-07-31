38c4a4e2639d761b11f38d40c57ff9c5cad91304
Michael Rico <rico.michael@gmail.com>
2016-07-31 12:23:24 -0700

    Implement linkedList methods and tests

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -16,16 +16,17 @@ if (typeof Object.prototype.extend !== 'function') {
 module.exports = (function () {
   var Node = function(value) {
     return {
-      head: value,
+      value: value,
       next: null
     }
   };
 
   var LinkedList = function() {
     var instance = {};
-    var _head = null;
-    var _tail = null;
-    var _size = 0;
+
+    instance._head = null;
+    instance._tail = null;
+    instance._size = 0;
     instance.extend(linkedListMethods);
 
     return instance;
@@ -33,24 +34,58 @@ module.exports = (function () {
 
   var linkedListMethods = {};
 
-  linkedListMethods.addToTail = function() {
-
+  linkedListMethods.addToTail = function(value) {
+    var node = Node(value);
+    if (!this._head) {
+      this._head = this._tail = node;
+    }
+    this._tail.next = node;
+    this._tail = this._tail.next;
+    this._size++;
+    return node;
   };
 
   linkedListMethods.removeHead = function() {
-
+    var output = this._head;
+    if (!output) {
+      return output;
+    }
+    this._head = this._head.next;
+    this._size--;
+    return output.value;
   };
 
-  linkedListMethods.remove = function() {
+  linkedListMethods.remove = function(value) {
+    // traverse linked list looking for the value
+    // if found, set previous next to current.next
+    var curr = this._head;
+    var prev = null;
+    var output;
 
+    while (curr) {
+      if (curr.value === value) {
+        if (!prev) {
+          this.removeHead();
+          return value;
+        }
+        output = curr.value;
+        prev.next = curr.next;
+        return output;
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+    return output;
   };
 
   linkedListMethods.size = function() {
-
+    return this._size;
   };
 
-  linkedListMethods.traverse = function() {
-
+  linkedListMethods.traverse = function(cb) {
+    for (var node = this._head; node; node = node.next) {
+      cb(node);
+    }
   };
 
   return LinkedList;
--- linkedList/linkedList.test.js
+++ linkedList/linkedList.test.js
@@ -18,7 +18,7 @@ describe('Linked List', function() {
 
   describe('removeHead', function() {
 
-    before(function() {
+    beforeEach(function() {
       linked_list.addToTail(4);
       linked_list.addToTail(6);
       linked_list.addToTail(8);
@@ -29,19 +29,59 @@ describe('Linked List', function() {
     });
 
     it('should report the correct size after removeing items', function() {
-      expect(linked_list.size()).to.eqaul(2);
+      expect(linked_list.size()).to.equal(3);
       linked_list.removeHead();
-      expect(linked_list.size()).to.eqaul(1);
+      expect(linked_list.size()).to.equal(2);
       linked_list.removeHead();
-      expect(linked_list.size()).to.eqaul(0);
+      expect(linked_list.size()).to.equal(1);
       linked_list.removeHead();
-      expect(linked_list.size()).to.eqaul(0);
+      expect(linked_list.size()).to.equal(0);
+      linked_list.removeHead();
+      expect(linked_list.size()).to.equal(0);
     });
 
     it('should report null for an empty list', function() {
+      expect(linked_list.size()).to.equal(3);
+      linked_list.removeHead();
+      expect(linked_list.size()).to.equal(2);
+      linked_list.removeHead();
+      expect(linked_list.size()).to.equal(1);
+      linked_list.removeHead();
+      expect(linked_list.size()).to.equal(0);
+      linked_list.removeHead();
+      expect(linked_list.size()).to.equal(0);
       expect(linked_list.removeHead()).to.equal(null);
+    });
+
+  });
+
+  describe('traverse', function() {
+    it('should execute a callback for every item in the list', function() {
+      var transformed = [];
+      linked_list.addToTail(1);
+      linked_list.addToTail(3);
+      linked_list.addToTail(5);
+      linked_list.traverse(function(node) {
+        transformed.push(node.value);
+      });
+      expect(transformed).to.eql([1, 3, 5]);
     });
   });
 
+  describe('remove', function() {
+    it('should remove an item', function() {
+      var transformed = [];
+      linked_list.addToTail('Mary');
+      linked_list.addToTail('had');
+      linked_list.addToTail('a');
+      linked_list.addToTail('little');
+      linked_list.addToTail('lamb');
+      linked_list.remove('a');
+      linked_list.traverse(function(node) {
+        transformed.push(node.value);
+      });
+      expect(transformed).to.eql(['Mary', 'had', 'little', 'lamb']);
+    });
+    });
 
 });
